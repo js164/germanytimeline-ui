9c56286c672945575b40d12058d67296636dd4bf
Jay Solanki <j194solanki@gmail.com>
2024-09-11 15:13:32 +0530

    OTP Verification

--- src/Components/Auth/login.jsx
+++ src/Components/Auth/login.jsx
@@ -5,54 +5,100 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setAlertShow, setAuth } from '../../ReduxStore/Action';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Login() {
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [otp, setOTP] = useState('')
+    const [showOTP, setShowOTP] = useState(false)
+    const [otpError, setOtpError] = useState('')
 
-  const  LogIn=(e)=> {
-    e.preventDefault();
-    let data = {
-        "username": username,
-        "password": password
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleOTPClose = () => setShowOTP(false);
+    const handleOTPShow = () => setShowOTP(true);
+
+    const LogIn = (e) => {
+        e.preventDefault();
+        let data = {
+            "email": email,
+            "password": password
+        }
+
+        axios.post('/auth/login', data).then((response) => {
+            console.log(response);
+            if (response && response.data && response.data.success) {
+                localStorage.setItem('email', response.data.user.email);
+                localStorage.setItem('userId', response.data.user.userId);
+                localStorage.setItem('isActive', response.data.user.isActive);
+                if(response.data.user.isActive){
+                    localStorage.setItem('access_token', response.data.user.access_token);
+                    localStorage.setItem('refresh_token', response.data.user.refresh_token);
+                    dispatch(setAuth())
+                    dispatch(setAlertShow('success', 'Congratulations!', response.data.message))
+                    navigate('/', { replace: true })
+                }else{
+                    handleOTPShow()
+                }
+            } else {
+                dispatch(setAlertShow('danger', 'Sorry!', response.data.message))
+            }
+        }).catch(err => {
+            dispatch(setAlertShow('danger', 'Sorry!', err.message))
+            console.log(err);
+        })
     }
 
-    axios.post('/auth/login', data).then((response) => {
-        console.log(response);
-        if (response.data.success) {
-            localStorage.setItem('username', response.data.user.username);
-            localStorage.setItem('access_token', response.data.user.access_token);
-            localStorage.setItem('refresh_token', response.data.user.refresh_token);
-            localStorage.setItem('userId', response.data.user.userId);
-            dispatch(setAuth())
-            dispatch(setAlertShow('success','Congratulations!',response.data.message))
-            navigate('/', { replace: true })
-        }else{
-            dispatch(setAlertShow('danger','Sorry!',response.data.message))
+    const verifyOTP = (e) => {
+        e.preventDefault();
+        const data = {
+            userId: localStorage.getItem('userId'),
+            email: localStorage.getItem('email'),
+            otp: otp
         }
-    }).catch(err => {
-        dispatch(setAlertShow('danger','Sorry!',err.message))
-        console.log(err);
-    })
-}
+        axios.post('/auth/verifyotp', data).then((response) => {
+            console.log(response);
+            if (response && response.data && response.data.success) {
+                localStorage.setItem('email', response.data.user.email);
+                localStorage.setItem('username', response.data.user.username);
+                localStorage.setItem('access_token', response.data.user.access_token);
+                localStorage.setItem('refresh_token', response.data.user.refresh_token);
+                localStorage.setItem('userId', response.data.user.userId);
+                localStorage.setItem('isActive', response.data.user.isActive);
+                dispatch(setAuth())
+                dispatch(setAlertShow('success','Congratulations!',response.data.message))
+                navigate('/', { replace: true })
+            } else {
+                setOtpError(response.data.message)
+                setOTP('')
+                setTimeout(()=>{
+                    setOtpError('')
+                },3000)
+            }
+        }).catch(err => {
+            dispatch(setAlertShow('danger','Sorry!',err.message))
+            console.log(err);
+        })
+    };
 
 
-  return (
-    <>
-      <div className='container'>
+
+    return (
+        <>
+            <div className='container'>
                 <h2 className='text-center'>Login</h2>
                 <Form onSubmit={LogIn}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" value={username} onChange={(e) => setUsername( e.target.value )} placeholder="Enter Username" />
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value )} placeholder="Password" />
+                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Log In
@@ -60,6 +106,21 @@ export default function Login() {
                     <Link to="/signup" className='m-2'>Create new account?</Link>
                 </Form>
             </div>
-    </>
-  )
+            <Modal show={showOTP} onHide={handleOTPClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Verify Your Email</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Please enter 6 digit code you received over your email.</Modal.Body>
+                <Form.Group className="mb-3 p-3" controlId="formBasicEmail">
+                    <Form.Control type="text" value={otp} onChange={(e) => setOTP(e.target.value)} placeholder="Enter OTP" />
+                </Form.Group>
+                <p style={{ color: "red" }} className='mx-3'>{otpError}</p>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={verifyOTP}>
+                        Verify
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
 }
--- src/Components/Auth/signup.jsx
+++ src/Components/Auth/signup.jsx
@@ -5,17 +5,27 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setAlertShow, setAuth } from '../../ReduxStore/Action';
+import Modal from 'react-bootstrap/Modal';
 
 
 export default function Signup(props) {
+    const [email, setEmail] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [otp, setOTP] = useState('')
+    const [showOTP, setShowOTP] = useState(false)
+    const [otpError, setOtpError] = useState('')
+
+    
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    
+    const handleOTPClose = () => setShowOTP(false);
+    const handleOTPShow = () => setShowOTP(true);
     const SignupUser = (e) => {
         e.preventDefault();
         let data = {
+            "email": email,
             "username": username,
             "password": password
         }
@@ -23,16 +33,48 @@ export default function Signup(props) {
         console.log((data));
         axios.post('/auth/signup', data).then((response) => {
             console.log(response);
-            if (response.data.success) {
+            if (response && response.data && response.data.success) {
+                localStorage.setItem('email', response.data.user.email);
+                localStorage.setItem('userId', response.data.user.userId);
+                localStorage.setItem('isActive', response.data.user.isActive);
+                // dispatch(setAuth())
+                dispatch(setAlertShow('success','Congratulations!',response.data.message))
+                // navigate('/', { replace: true })
+                handleOTPShow()
+            } else {
+                dispatch(setAlertShow('danger','Sorry!',response.data.message))
+            }
+        }).catch(err => {
+            dispatch(setAlertShow('danger','Sorry!',err.message))
+            console.log(err);
+        })
+    };
+
+    const verifyOTP = (e) => {
+        e.preventDefault();
+        const data = {
+            userId: localStorage.getItem('userId'),
+            email: localStorage.getItem('email'),
+            otp: otp
+        }
+        axios.post('/auth/verifyotp', data).then((response) => {
+            console.log(response);
+            if (response && response.data && response.data.success) {
+                localStorage.setItem('email', response.data.user.email);
                 localStorage.setItem('username', response.data.user.username);
                 localStorage.setItem('access_token', response.data.user.access_token);
                 localStorage.setItem('refresh_token', response.data.user.refresh_token);
                 localStorage.setItem('userId', response.data.user.userId);
+                localStorage.setItem('isActive', response.data.user.isActive);
                 dispatch(setAuth())
                 dispatch(setAlertShow('success','Congratulations!',response.data.message))
                 navigate('/', { replace: true })
             } else {
-                dispatch(setAlertShow('danger','Sorry!',response.data.message))
+                setOtpError(response.data.message)
+                setOTP('')
+                setTimeout(()=>{
+                    setOtpError('')
+                },3000)
             }
         }).catch(err => {
             dispatch(setAlertShow('danger','Sorry!',err.message))
@@ -47,9 +89,14 @@ export default function Signup(props) {
         <div className='container'>
             <h2 className='text-center'>SignUp</h2>
             <Form onSubmit={SignupUser}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter Username" />
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter name" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -62,6 +109,21 @@ export default function Signup(props) {
                 <Link to="/login" className='m-2'>Alredy have account?</Link>
             </Form>
         </div>
+        <Modal show={showOTP} onHide={handleOTPClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Verify Your Email</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Please enter 6 digit code you received over your email.</Modal.Body>
+        <Form.Group className="mb-3 p-3" controlId="formBasicEmail">
+                    <Form.Control type="text" value={otp} onChange={(e) => setOTP(e.target.value)} placeholder="Enter OTP" />
+            </Form.Group>
+        <p style={{color:"red"}} className='mx-3'>{otpError}</p>    
+        <Modal.Footer>
+          <Button variant="primary" onClick={verifyOTP}>
+            Verify
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </>
     )
 }
